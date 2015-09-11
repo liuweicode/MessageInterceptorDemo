9be33093673bedf92b8e44c23aea04373525a6dc
liuwei <liuwei@noahwm.com>
2015-09-11 13:32:12 +0800

    Initial Commit

---
+++ MessageInterceptorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageInterceptorDemo
+//
+//  Created by William on 15/9/11.
+//  Copyright (c) 2015年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageInterceptorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MessageInterceptorDemo
+//
+//  Created by William on 15/9/11.
+//  Copyright (c) 2015年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
